01ac5fea89b4594f07aed919c54904a936a3b37d
Caspar Fuller <Caspar.Fuller@softwire.com>
2018-07-11 11:23:28 +0100

    Tweaks to part 1 after review

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const readline = require('readline-sync');
 
-const API_KEY = '?app_id=66354b18&app_key=e9af9af78d300a4ee2674b1cef0333cc';
+const API_QUERY_STRING_TFL = '?app_id=66354b18&app_key=e9af9af78d300a4ee2674b1cef0333cc';
 
 function main() {
 
@@ -9,7 +9,7 @@ function main() {
 
     const stopCode = readline.prompt();
 
-    const url = 'https://api.tfl.gov.uk/StopPoint/' + stopCode + '/Arrivals' + API_KEY;
+    const url = `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals${API_QUERY_STRING_TFL}`;
 
     request(url, function (error, response, body) {
 
@@ -18,7 +18,7 @@ function main() {
         } else {
             const rawData = JSON.parse(body);
             const sortedData = sortData(rawData);
-            displayData(sortedData);
+            displayBuses(sortedData);
         }
     });
 
@@ -27,22 +27,20 @@ function main() {
 
 function sortData(rawData) {
 
-    rawData.sort(function (a, b) {
-        return a['timeToStation'] > b['timeToStation'];
-    });
+    rawData.sort((a, b) => a['timeToStation'] > b['timeToStation']);
     return rawData.slice(0, 5);
 }
 
-function displayData(sortedData) {
-
-    for (let busData in sortedData) {
-        console.log('Bus Route:', sortedData[busData]["lineId"]);
-        console.log('Destination:', sortedData[busData]["destinationName"]);
-        let minutes = Math.floor((sortedData[busData]["timeToStation"]) / 60);
-        let seconds = (sortedData[busData]["timeToStation"]) % 60;
+function displayBuses(buses) {
+    for (let bus of buses) {
+        console.log('Bus Route:', bus["lineId"]);
+        console.log('Destination:', bus["destinationName"]);
+        const minutes = Math.floor((bus["timeToStation"]) / 60);
+        const seconds = (bus["timeToStation"]) % 60;
         console.log('Arriving in:', minutes, "minutes and", seconds, "seconds\n");
     }
 
 }
 
 main();
+
